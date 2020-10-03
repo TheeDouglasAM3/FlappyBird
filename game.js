d2931ca11bd5516ab615f1dab15be2b9bb86f48a
Douglas Marcelino <douglas_champion@hotmail.com>
2020-10-02 22:34:29 -0300

    Add rotate bird and move foreground logics game.js

--- game.js
+++ game.js
@@ -4,6 +4,7 @@ const ctx = cvs.getContext('2d')
 
 /**GAME VARS AND CONSTS */
 let frames = 0
+const DEGREE = Math.PI / 180
 
 /**LOAD SPRITE IMAGE */
 const sprite = new Image()
@@ -59,11 +60,18 @@ const fg = {
   w: 224,
   h: 112,
   x: 0,
-  y: cvs.height - 102,
+  y: cvs.height - 112,
+
+  dx: 2,
 
   draw: function() {
     ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h)
     ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, (this.x + this.w), this.y, this.w, this.h)
+  },
+
+  update: function() {
+    if(state.current == state.game) 
+      this.x = (this.x - this.dx) % (this.w / 2)
   }
 }
 
@@ -84,11 +92,17 @@ const bird = {
   gravity: 0.25,
   jump: 4.6,
   speed: 0,
+  rotation: 0,
 
   draw: function() {
     let bird = this.animation[this.frame]
 
-    ctx.drawImage(sprite, bird.sX, bird.sY, this.w, this.h, this.x, this.y, this.w, this.h)
+    ctx.save()
+    ctx.translate(this.x, this.y)
+    ctx.rotate(this.rotation)
+    ctx.drawImage(sprite, bird.sX, bird.sY, this.w, this.h,- this.w/2, - this.h/2, this.w, this.h)
+
+    ctx.restore()
   },
 
   flap: function() {
@@ -111,6 +125,7 @@ const bird = {
       //RESET BIRD POSITION AFTER GAME OVER
       this.y = 150
       this.speed = 0
+      this.rotation = 0 * DEGREE
 
     }else{
       this.speed += this.gravity
@@ -120,6 +135,14 @@ const bird = {
         this.y = cvs.height - fg.h - (this.h / 2)
         if(state.current == state.game)
           state.current = state.over
+      }
+
+      //if the speed is greater than the jump means the bird is falling down
+      if(this.speed >= this.jump) {
+        this.rotation = 90 * DEGREE
+        this.frame = 1
+      }else{
+        this.rotation = -25 * DEGREE
       }
     }
   },
@@ -170,6 +193,7 @@ function draw() {
 /**UPDATE */
 function update() {
   bird.update()
+  fg.update()
 }
 
 /**LOOP */

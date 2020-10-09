f5a4ab648c87b0ac09b853a8cd39f087d6a68753
Douglas Marcelino <douglas_champion@hotmail.com>
2020-10-09 17:49:50 -0300

    Update font styles

--- game.js
+++ game.js
@@ -332,18 +332,18 @@ const score = {
   value: 0,
 
   draw : function(){
-    ctx.fillStyle = "#FFF";
-    ctx.strokeStyle = "#000";
+    ctx.fillStyle = "#e0d42b";
+    ctx.strokeStyle = "#121212";
     
     if(state.current == state.game){
         ctx.lineWidth = 2;
-        ctx.font = "35px Teko";
-        ctx.fillText(this.value, cvs.width / 2, 50)
-        ctx.strokeText(this.value, cvs.width / 2, 50)
+        ctx.font = "bold 35px Teko";
+        ctx.fillText(this.value, cvs.width / 2 -10, 50)
+        ctx.strokeText(this.value, cvs.width / 2 -10, 50)
         
     }else if(state.current == state.over){
         // SCORE VALUE
-        ctx.font = "25px Teko"
+        ctx.font = "bold 25px Teko"
         ctx.fillText(this.value, 225, 186)
         ctx.strokeText(this.value, 225, 186)
         // BEST SCORE

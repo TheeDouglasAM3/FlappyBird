a761da1ada51a2c956b5acf8774b8b3814e7d5dd
Douglas Marcelino <douglas_champion@hotmail.com>
2020-10-12 22:50:10 -0300

    Add music for the easteregg level

--- theme.js
+++ theme.js
@@ -1,9 +1,14 @@
-function playTheme(){
-  const music = document.getElementById("myAudio"); 
-  music.play();
-}
+const mainTheme = document.getElementById("myAudio");
+const demonTheme = document.getElementById("myAudio2");
 
 let canvas = document.querySelector('canvas')
 canvas.addEventListener('click', () => {
-  playTheme()
+  if(score.value < 100) {
+    mainTheme.play()
+    demonTheme.pause()
+  }else{
+    mainTheme.pause()
+    demonTheme.play()
+  }
+ 
 })

3d62132887240ff527645942ef7889bd29f5fb82
Douglas Marcelino <douglas_champion@hotmail.com>
2020-10-12 23:44:05 -0300

    Update volume themes

--- theme.js
+++ theme.js
@@ -1,5 +1,7 @@
 const mainTheme = document.getElementById("myAudio");
 const demonTheme = document.getElementById("myAudio2");
+mainTheme.volume = 0.4
+demonTheme.volume = 0.6
 
 let canvas = document.querySelector('canvas')
 canvas.addEventListener('click', () => {
